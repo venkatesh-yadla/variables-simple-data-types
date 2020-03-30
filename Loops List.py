df3abe84f86175f3084a8a3487a3beb4507fb13d
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-03-30 22:59:47 +0530

    Create Loops List.py

---
+++ Loops List.py
@@ -0,0 +1,25 @@
+fruits=['apple','banana','grapes']
+for fruit in fruits:
+    print (fruit)
+    print (fruits.title()+",is so sweet")
+print ("Thank you for fruits")
+
+#Numerical Lists
+for value in range(1,5):
+   print (value)
+   
+#Range()
+numbers=list(range(1,5))
+print (numbers)
+
+#Odd numbers
+odd_numbers=list(range(1,20))
+print (odd_numbers)
+
+#cubes 
+cubes=[value**3 for value in range(3,30)]
+print (cubes)
+
+#6th table
+for i in range(1,11)
+    print (i"*6")
