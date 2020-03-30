46fd1999fe3f62e8d317e6d7ee09a5708c4a2c48
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-03-30 23:48:27 +0530

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,17 @@
+colours=(red,blue,green)
+print(colours[1])
+print(colours[-1])
+
+#for loop
+for colour in colours:
+    print(colour)
+    
+#over writing
+colours=(red,blue)
+print ("old colours")
+for colour in colours:
+    print(colour)
+colours=(pink,black)
+print("new colours")
+for colour in colours:
+    print(colour)
