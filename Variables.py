c4cd7a60b9d966cc7624845af730f33351fa623f
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-03-30 22:05:43 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,18 @@
+name ="sathyabama"
+print(name)
+print(name.title())
+print(name.lower())
+print(name.upper())
+first_name="Corona"
+last_name="virus"
+full_name=first_name+" "+last_name
+print(full_name)
+print("dangerous,"+full_name.title())
+print("Python")
+print("\t Python")
+print("Groups:\nCSE\nECE\nEEE")
+friut_name=" Apple "
+print(fruit_name)
+fruit_name=fruit_name.rstrip()
+fruit_name=fruit_name.lsrtip()
+fruit_name=fruit_name.strip()
