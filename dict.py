010cc6eaabb1dea3b8b8eeb159516f95bf754c47
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-03-31 23:29:43 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,7 @@
+users={'colours':{'pink':'baby','black':'toy'}, 'fruits':{'pink':'sweet','black':'tasty'}}
+for corona,virus in users.items():
+    print ("\n corona:"+corona)
+    pink=virus["pink"]
+    black=virus["black"]
+    print ("\t pink:"+pink.title())
+    print ("\t black:"+black.title())
