737e25d1a8a790d33ccf8a3ec62797dea23adaff
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-03-31 23:28:35 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,11 @@
+fruits=['apple','banana','grapes']
+print(fruits [0:2])
+fruits.append('papaya')
+print (fruits)
+print (fruits [1:3])
+print (fruits [ :2])
+print (fruits [3: ])
+flowers=fruits
+print (flowers)
+for fruit in fruits [:2]:
+    print (fruit)
