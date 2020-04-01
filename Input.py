6b1b64caeef66f447daf4e481be8cee02bc4184e
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-04-01 22:06:27 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,11 @@
+#input() function
+message = input("Enter your name: ")
+print(message)
+
+#int() function
+height = int(input("How tall are you, in inches? "))
+if height >= 36:
+    print("\nYou're tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
+  
