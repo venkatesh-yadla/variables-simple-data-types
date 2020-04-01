594b9e659fd43d7a55a531597d534e3ef651acd8
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-04-01 21:32:01 +0530

    Create Conditionals.py

---
+++ Conditionals.py
@@ -0,0 +1,25 @@
+#if-else statement
+colours=['red','blue','green','yellow']
+for colour in colours:
+    if colour =='green':
+       print (colour.title())
+    else:
+       print (colour.upper())
+print ("do it again")
+
+ #if statement
+ age=20
+ if age >=18:
+    print("you are major")
+    
+  # if-elif-else statement
+  age=20
+  if age<12:
+     print ("you are child")
+  elif age<18:
+     print ("you are minor")
+  else:
+     print ("you are major")
+     
+     
+
