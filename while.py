1c690cd6a5b47e50041df3e47f7fa682e292e3e7
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-04-01 22:13:23 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,13 @@
+roll_no=1
+while roll_no<=6:
+    print (roll_no)
+    roll_no+=1
+    
+#continue
+roll_no=1
+while roll_no<=6:
+    print (roll_no)
+    roll_no+=1
+    if roll_no%2==0:
+        continue
+        print (roll_no)
