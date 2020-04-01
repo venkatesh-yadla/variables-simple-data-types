26c917e4effe50a678238eac4c827d0f6bb289f8
venkatesh-yadla <62637250+venkatesh-yadla@users.noreply.github.com>
2020-04-01 21:57:51 +0530

    Add files via upload

---
+++ Nesting.py
@@ -0,0 +1,19 @@
+#list of dictionary
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+#list in dictionary
+favorite_languages = {
+  'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+    }
+for name, languages in favorite_languages.items():
+    print("\n" + name.title() + "'s favorite languages are:")
+for language in languages:
+        print("\t" + language.title())
+
